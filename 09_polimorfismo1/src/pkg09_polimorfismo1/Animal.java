e1d20760d17de580d54e07b592edd7c00fba0e8a
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2024-02-27 09:05:12 -0600

    commit 2702202413136

---
+++ 09_polimorfismo1/src/pkg09_polimorfismo1/Animal.java
@@ -0,0 +1,22 @@
+package pkg09_polimorfismo1;
+
+/**
+ * Representa a nu animal generico
+ * @author Gerardo Portillo
+ */
+public class Animal {
+    //atributos
+    private String nombre;
+    private String especie;
+    private int patas;
+    private boolean tieneCola;
+    //constructor
+    public Animal(String nombre, String especie, int patas, boolean tieneCola) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.patas = patas;
+        this.tieneCola = tieneCola;
+    }
+    //metodos
+    //AQUI CONTINUAREMOS
+}
